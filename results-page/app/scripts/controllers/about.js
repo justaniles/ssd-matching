8848fd37a10658bd66da251de8b1be849e2bd29d
Justin Niles <justin.niles12@gmail.com>
2015-10-21 21:13:23 -0400

    Switch to yeoman generated app

---
+++ results-page/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resultsPageApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the resultsPageApp
+ */
+angular.module('resultsPageApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
