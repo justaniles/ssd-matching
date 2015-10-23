a23a14d2d924f8bb6a06b3d2b6fbe1a90c49ff63
Cody Cahoon <codyc@vt.edu>
2015-10-23 12:01:05 -0400

    Formats database results to json

--- server/insert.php
+++ server/insert.php
@@ -8,21 +8,25 @@ $link = mysqli_connect("localhost", "root", "password", "ssd");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
-// Escape user inputs for security
-$first_name = mysqli_real_escape_string($link, $_POST['firstname']);
-$last_name = mysqli_real_escape_string($link, $_POST['lastname']);
-$dpt = mysqli_real_escape_string($link, $_POST['dpt']);
-$course = mysqli_real_escape_string($link, $_POST['course']);
+
+$first = mysqli_real_escape_string($link, $_POST['nameFirst']);
+$last = mysqli_real_escape_string($link, $_POST['nameLast']);
+$email = mysqli_real_escape_string($link, $_POST['nameFirst']);
+$dpt = mysqli_real_escape_string($link, $_POST['subject']);
+$course = mysqli_real_escape_string($link, $_POST['number']);
 $crn = mysqli_real_escape_string($link, $_POST['crn']);
-$email = mysqli_real_escape_string($link, $_POST['email']); 
+$instructor = mysqli_real_escape_string($link, $_POST['instructor']);
+$instructor_email = mysqli_real_escape_string($link, $_POST['instructorEmail']);
+$notetaker = false;
+$assigned = false;
 
-$database = 'notetaker';
+//$email = "sample@vt.edu";
 
+echo $course;
 
+$sql = "INSERT INTO student (FIRSTNAME, LASTNAME, EMAIL, DEPARTMENT, COURSE_NUMBER, CRN, INSTRUCTOR, INSTRUCTOR_EMAIL, IS_NOTETAKER, IS_ASSIGNED) VALUES ('$first', '$last', '$email', '$dpt', '$course', '$crn', '$instructor', '$instructor_email', '$notetaker', '$assigned')";
 
 // attempt insert query execution
-$sql = "INSERT INTO $database (FIRSTNAME, LASTNAME, DEPARTMENT, COURSE, CRN, EMAIL) VALUES ('$first_name', '$last_name', '$dpt', '$course', '$crn', '$email')";
 if(mysqli_query($link, $sql)){
    //echo "Records added successfully.";
 } else{
---
+++ server/pull.php
@@ -0,0 +1,125 @@
+
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<?php
+
+// Connect to mysql db at localhost to db ssd
+$link = mysqli_connect("localhost", "root", "password", "ssd");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$statement = "SELECT * FROM student ORDER BY student.CRN, student.COURSE_NUMBER";
+$query = mysqli_query($link, $statement);
+
+$prevCRN = -1;
+$department = '';
+$course = '';
+$crn = '';
+$instructor = '';
+$instructor_email = '';
+$requesters = [];
+$notetakers = [];
+$classes = [];
+
+
+while($row = mysqli_fetch_assoc($query))
+{
+	$crn = $row['CRN'];
+	
+	//first call
+	if ($prevCRN == -1)
+	{
+		$prevCRN = $crn;
+		$department = $row['DEPARTMENT'];
+		$course = $row['COURSE_NUMBER'];
+		$instructor = $row['INSTRUCTOR'];
+		$instructor_email = $row['INSTRUCTOR_EMAIL'];
+
+		$is_notetaker = $row['IS_NOTETAKER'];
+		$newstudent = array('firstName' => $row['FIRSTNAME'], 'lastName' => $row['LASTNAME'], 'email' => $row['EMAIL']);
+		$studentString = json_encode($newstudent);
+
+		//if is a requester, add to requester list
+		if ($is_notetaker == "0")
+		{
+			array_push($requesters, $studentString);
+		}
+		else
+		{
+			array_push($notetakers, $studentString);
+		}
+	}
+	//new crn
+	else if ($prevCRN != $crn)
+	{
+		$requesterString = json_encode($requesters);
+		$notetakerString = json_encode($notetakers);
+		$class = array('crn' => $prevCRN, 'department' => $department, 'courseNumber' => $course, "instructor" => $instructor, "instructorEmail" => $instructor_email, 'requesters' => $requesterString, 'notetakers' => $notetakerString);
+
+		array_push($classes, $class);
+
+		unset($requesters);
+		unset($notetakers);
+
+		$requesters = [];
+		$notetakers = [];
+
+		$prevCRN = $crn;
+		$department = $row['DEPARTMENT'];
+		$course = $row['COURSE_NUMBER'];
+		$instructor = $row['INSTRUCTOR'];
+		$instructor_email = $row['INSTRUCTOR_EMAIL'];
+
+		$is_notetaker = $row['IS_NOTETAKER'];
+		$newstudent = array('firstName' => $row['FIRSTNAME'], 'lastName' => $row['LASTNAME'], 'email' => $row['EMAIL']);
+		$studentString = json_encode($newstudent);
+
+		//if is a requester, add to requester list
+		if ($is_notetaker == "0")
+		{
+			array_push($requesters, $studentString);
+		}
+		else
+		{
+			array_push($notetakers, $studentString);
+		}
+	}
+	//same crn
+	else
+	{
+		$is_notetaker = $row['IS_NOTETAKER'];
+		$newstudent = array('firstName' => $row['FIRSTNAME'], 'lastName' => $row['LASTNAME'], 'email' => $row['EMAIL']);
+		$studentString = json_encode($newstudent);
+
+		//if is a requester, add to requester list
+		if ($is_notetaker == '0')
+		{
+			array_push($requesters, $studentString);
+		}
+		else
+		{
+			array_push($notetakers, $studentString);
+		}
+	}
+}
+
+$class = array('crn' => $prevCRN, 'department' => $department, 'courseNumber' => $course, "instructor" => $instructor, "instructorEmail" => $instructor_email, 'requesters' => $requesterString, 'notetakers' => $notetakerString);
+array_push($classes, $class);
+
+echo str_replace("\\", "", json_encode($classes));
+ 
+// close connection
+mysqli_close($link);
+
+?>
+
+
+
+
+
+
