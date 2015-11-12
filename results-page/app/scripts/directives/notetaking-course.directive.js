1f8ab9a941eaa2170aa0407ec96fe46cbc317dbb
Justin Niles <justin.niles12@gmail.com>
2015-11-12 13:57:56 -0500

    Send http request when checkbox is toggled

--- results-page/app/scripts/directives/notetaking-course.directive.js
+++ results-page/app/scripts/directives/notetaking-course.directive.js
@@ -1,6 +1,6 @@
 angular.module("ResultsPageApp")
 
-.directive("notetakingCourse", function($timeout, managerService) {
+.directive("notetakingCourse", function($timeout, $http, managerService) {
   return {
     restrict: "C",
     scope: {
@@ -10,9 +10,33 @@ angular.module("ResultsPageApp")
 
       $scope.showingSummary = true;
 
-      $scope.assignmentToggle = function assignmentToggle(requester) {
-        console.log(JSON.stringify(requester));
-      }
+      $scope.assignmentToggle = function assignmentToggle(student, isNotetaker) {
+        console.log(JSON.stringify(student));
+        var data = {
+          firstname: student.firstName,
+          lastname: student.lastName,
+          email: student.email,
+          department: $scope.courseInfo.department,
+          coursenumber: $scope.courseInfo.courseNumber,
+          crn: $scope.courseInfo.crn,
+          instructor: $scope.courseInfo.instructor,
+          instructoremail: $scope.courseInfo.instructorEmail,
+          isnotetaker: isNotetaker,
+          isassigned: student.isAssigned
+        };
+        $http({
+          method: "GET",
+          url: "http://localhost/assign.php",
+          params: data
+        }).then(
+          function success(response) {
+
+          },
+          function failure(response) {
+
+          }
+        );
+      };
 
       $scope.toggleIsSummary = function toggleIsSummary(event) {
         $timeout(function() {
