3d694d1f2f7cf79a101669e13e3239877de9c265
Justin Niles <justin.niles12@gmail.com>
2015-12-03 19:14:34 -0500

    Update buildAll.sh

--- buildAll.sh
+++ buildAll.sh
@@ -3,9 +3,12 @@ sleep 3
 cd results-page
 echo "Running npm install..."
 npm install
+
 echo "Running bower install..."
 bower install
+
 echo "Running grunt build..."
 grunt build
+
 echo "All done! The built results-page can be found in results-page/dist"
 cd ..
